ff8139949acf0a873b6d7281f09d4c3a258fda49
VishnuPriya2297 <davu.vishnupriya2297@gmail.com>
2019-07-25 11:53:41 +0530

    added application properties

--- src/main/java/com/stackroute/Muzix/MuzixApplication.java
+++ src/main/java/com/stackroute/Muzix/MuzixApplication.java
@@ -1,13 +1,69 @@
 package com.stackroute.Muzix;
 
+import com.stackroute.Muzix.domain.Track;
+import com.stackroute.Muzix.exceptions.TrackAlreadyExsistsException;
+import com.stackroute.Muzix.repository.TrackRepository;
+import com.stackroute.Muzix.service.TrackService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-public class MuzixApplication {
+@Component
+public class MuzixApplication  implements ApplicationListener<ContextRefreshedEvent>, CommandLineRunner{
+
+	@Value("15")
+	private int Id;
+
+	@Value("meghana")
+	private String name;
+
+	@Value("good")
+	private String comment;
+
+	@Autowired
+	TrackRepository trackRepository;
+
+	@Autowired
+	Environment environment;
+
+	@Autowired
+	TrackService trackService;
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MuzixApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		try
+		{
+			trackService.saveTrack(new Track(22,"poojitha","bad"));
+		}
+		catch (TrackAlreadyExsistsException e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+		try
+		{
+			trackService.saveTrack(new Track(11,"pallavi","thin"));
+		}
+		catch (TrackAlreadyExsistsException e)
+		{
+			e.printStackTrace();
+		}
+
+	}
 }
