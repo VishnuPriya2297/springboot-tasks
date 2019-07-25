36c184a59ebec01206d65f86189b0760a837999a
VishnuPriya2297 <davu.vishnupriya2297@gmail.com>
2019-07-25 10:38:14 +0530

    lombok task

--- src/main/java/com/stackroute/Muzix/domain/Track.java
+++ src/main/java/com/stackroute/Muzix/domain/Track.java
@@ -1,24 +1,34 @@
 package com.stackroute.Muzix.domain;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Track {
-    public Track(int id, String name, String comment) {
-        this.id = id;
-        this.name = name;
-        this.comment = comment;
-    }
+
 
     @Id
     private int id;
     private String name;
     private String comment;
 
-    public Track() {
+    /*public Track() {
     }
-
+    public Track(int id, String name, String comment) {
+        this.id = id;
+        this.name = name;
+        this.comment = comment;
+    }
 
     public int getId() {
         return id;
@@ -51,5 +61,5 @@ public class Track {
                 ", name='" + name + '\'' +
                 ", comment='" + comment + '\'' +
                 '}';
-    }
+    }*/
 }
