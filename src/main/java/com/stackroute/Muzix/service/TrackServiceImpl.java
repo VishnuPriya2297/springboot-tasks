14afe9a7efa5c454eef349d0675ac5d265064e4c
VishnuPriya2297 <51954142+VishnuPriya2297@users.noreply.github.com>
2019-08-07 11:25:02 +0530

    used proper naming conventions

--- src/main/java/com/stackroute/Muzix/service/TrackServiceImpl.java
+++ src/main/java/com/stackroute/Muzix/service/TrackServiceImpl.java
@@ -41,7 +41,7 @@ public class TrackServiceImpl implements TrackService {
     }
 
     @Override
-    public Track UpdateTrack(Track track) {
+    public Track updateTrack(Track track) {
         Track updatedTrack=trackRepository.save(track);
         return updatedTrack;
     }
