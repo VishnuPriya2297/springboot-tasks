2b25fca014e616d7fa89e668c6861c2d382eb55a
VishnuPriya2297 <51954142+VishnuPriya2297@users.noreply.github.com>
2019-08-07 11:24:04 +0530

    used proper naming conventions

--- src/main/java/com/stackroute/Muzix/service/TrackService.java
+++ src/main/java/com/stackroute/Muzix/service/TrackService.java
@@ -14,7 +14,7 @@ public interface TrackService {
 
     public Track getTrackById(int id);
 
-    public Track UpdateTrack(Track track);
+    public Track updateTrack(Track track);
 
     public List<Track> getTrackByName(String name);
 
